c824c461e87fdf652c391579c61b04ad352f8831
Ricky Suprayudi <juraganled@gmail.com>
2024-04-15 01:07:22 +0700

    button font size, add information script

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ document.addEventListener("DOMContentLoaded", function() {
     var angleDeg = 0;
     const buttonLeft = document.getElementById("buttonLeft");
     const buttonRight = document.getElementById("buttonRight");
-    const imageOuter = document.getElementById("imageOuter");
-    const imageLogo = document.getElementById("imageLogo");
     const imageInner = document.getElementById("imageInner");
+    const information = document.getElementById("information");
+    const wheel = document.getElementById("wheel");
+    const buttonInfo = document.getElementById("buttonInfo");
+    const buttonOK = document.getElementById("buttonOK");
   
     buttonLeft.addEventListener("click", function() {
         angleDeg = angleDeg - 13.846153846153846;
@@ -16,5 +18,14 @@ document.addEventListener("DOMContentLoaded", function() {
         imageInner.style.transform = "translate(-50%, -50%) rotate(" + angleDeg + "deg)";
         console.log("angle = " + angleDeg);
     });
+    buttonInfo.addEventListener("click", function() {
+        wheel.style.display = "none";
+        information.style.display = "block";
+    });
+    buttonOK.addEventListener("click", function() {
+        wheel.style.display = "block";
+        information.style.display = "none";
+    });
+
 });
 
