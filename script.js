d10549ab32b6afd5cbe13631051185ebbc9d2acf
Ricky Suprayudi <juraganled@gmail.com>
2024-04-15 00:23:42 +0700

    css rotation and centered

--- script.js
+++ script.js
@@ -1,45 +1,20 @@
-const degrees = ["13.8462deg",
-"27.6923deg",
-"41.5385deg",
-"55.3846deg",
-"69.2308deg",
-"83.0769deg",
-"96.9231deg",
-"110.7692deg",
-"124.6154deg",
-"138.4615deg",
-"152.3077deg",
-"166.1538deg",
-"180.0000deg",
-"193.8462deg",
-"207.6923deg",
-"221.5385deg",
-"235.3846deg",
-"249.2308deg",
-"263.0769deg",
-"276.9231deg",
-"290.7692deg",
-"304.6154deg",
-"318.4615deg",
-"332.3077deg",
-"346.1538deg"];
-
-
 document.addEventListener("DOMContentLoaded", function() {
-    var angle = 0;
+    var angleDeg = 0;
     const buttonLeft = document.getElementById("buttonLeft");
     const buttonRight = document.getElementById("buttonRight");
+    const imageOuter = document.getElementById("imageOuter");
+    const imageLogo = document.getElementById("imageLogo");
     const imageInner = document.getElementById("imageInner");
   
     buttonLeft.addEventListener("click", function() {
-        angle = angle - 13.846153846153846;
-        imageInner.style.transform = "rotate(" + angle + "deg)";
-        console.log("angle = " + angle);
+        angleDeg = angleDeg - 13.846153846153846;
+        imageInner.style.transform = "translate(-50%, -50%) rotate(" + angleDeg + "deg)";
+        console.log("angle = " + angleDeg);
     });
     buttonRight.addEventListener("click", function() {
-        angle = angle + 13.846153846153846;
-        imageInner.style.transform = "rotate(" + angle + "deg)";
-        console.log("angle = " + angle);
+        angleDeg = angleDeg + 13.846153846153846;
+        imageInner.style.transform = "translate(-50%, -50%) rotate(" + angleDeg + "deg)";
+        console.log("angle = " + angleDeg);
     });
 });
 
