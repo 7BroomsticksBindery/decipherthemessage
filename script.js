ce79c83eccaf519c2f55b8598ab75834c1eb2202
Ricky Suprayudi <juraganled@gmail.com>
2024-06-09 18:13:44 +0700

    add js dom screen orientation and resize so that the key and cypher message can be displayed correctly by screen orientation

--- script.js
+++ script.js
@@ -7,7 +7,17 @@ document.addEventListener("DOMContentLoaded", function() {
     const wheel = document.getElementById("wheel");
     const buttonInfo = document.getElementById("buttonInfo");
     const buttonOK = document.getElementById("buttonOK");
-  
+    const contentPortrait = document.getElementById("contentPortrait");
+    const contentLandscape = document.getElementById("contentLandscape");
+
+    if (window.innerHeight > window.innerWidth) {
+        contentPortrait.style.display = "block";
+        contentLandscape.style.display = "none";
+    } else {
+        contentPortrait.style.display = "none";
+        contentLandscape.style.display = "block";
+    }
+
     buttonLeft.addEventListener("click", function() {
         angleDeg = angleDeg - 13.846153846153846;
         imageInner.style.transform = "translate(-50%, -50%) rotate(" + angleDeg + "deg)";
@@ -25,6 +35,26 @@ document.addEventListener("DOMContentLoaded", function() {
     buttonOK.addEventListener("click", function() {
         wheel.style.display = "block";
         information.style.display = "none";
+    });
+
+    window.addEventListener("orientationchange", function() {
+        if (window.innerHeight > window.innerWidth) {
+            contentPortrait.style.display = "block";
+            contentLandscape.style.display = "none";
+        } else {
+            contentPortrait.style.display = "none";
+            contentLandscape.style.display = "block";
+        }
+    });
+
+    window.addEventListener("resize", function() {
+        if (window.innerHeight > window.innerWidth) {
+            contentPortrait.style.display = "block";
+            contentLandscape.style.display = "none";
+        } else {
+            contentPortrait.style.display = "none";
+            contentLandscape.style.display = "block";
+        }
     });
 
 });
